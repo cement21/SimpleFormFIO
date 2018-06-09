4c479735365b98798f1a4c08877407a200d68804
cement21 <cement21@yandex.ru>
2018-06-09 17:42:51 +0700

    Create new Project

---
+++ Form.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+public class Form {
+    private JPanel rootPanel;
+    private JTextField textFieldSurname;
+    private JLabel labelSurname;
+    private JTextField textFieldPatronymic;
+    private JTextField textFieldFirstName;
+    private JLabel labelFirstName;
+    private JLabel labelPatronymic;
+    private JButton button;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JLabel labelBirthday;
+    private JLabel labelCyti;
+
+    public JPanel getRootPanel() {
+        return rootPanel;
+    }
+}
---
+++ Loader.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Loader {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        Form form = new Form();
+        frame.setContentPane(form.getRootPanel());
+        frame.setTitle("Fio");
+        frame.setSize(800, 600);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
